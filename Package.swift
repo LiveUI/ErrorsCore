89c79697282c3d5543f20861f4e9cf941e991c50
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-26 14:18:15 +0100

    initial upload

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "ErrorsCore",
+    products: [
+        .library(name: "ErrorsCore", targets: ["ErrorsCore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2")
+    ],
+    targets: [
+        .target(name: "ErrorsCore", dependencies: ["Vapor"]),
+        .testTarget(name: "ErrorsCoreTests", dependencies: ["ErrorsCore", "Vapor"]),
+    ]
+)
---
+++ Tests/ErrorsCoreTests/ErrorsCoreTests.swift
@@ -0,0 +1,16 @@
+import App
+import Dispatch
+import XCTest
+
+
+final class ErrorsCoreTests : XCTestCase {
+    
+    func testNothing() throws {
+        XCTAssert(true)
+    }
+    
+    static let allTests = [
+        ("testNothing", testNothing),
+        ]
+    
+}
