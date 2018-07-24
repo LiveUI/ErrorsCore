8fe09ed43c658c40e3d57d129cda85f15fcc7fbc
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-07-24 12:59:16 +0100

    update

--- Sources/ErrorsCore/Protocols/FrontendError.swift
+++ Sources/ErrorsCore/Protocols/FrontendError.swift
@@ -14,17 +14,3 @@ public protocol FrontendError: Debuggable {
     var status: HTTPStatus { get }
 }
 
-
-extension FrontendError {
-    
-    @available(*, deprecated, renamed: "identifier")
-    public var code: String {
-        return identifier
-    }
-    
-    @available(*, deprecated, renamed: "reason")
-    public var desctiption: String {
-        return reason
-    }
-    
-}
